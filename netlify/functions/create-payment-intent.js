48a32e24fab75c4648affab7004db05cf9c04492
Daniel <danielsein77@gmail.com>
2024-01-15 19:43:47 -0500

    payment form

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const stripe = require("stripe")(process.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 exports.handler = async (event) => {
 	try {
-		const { amount } = JSON.parce(event.body);
+		const { amount } = JSON.parse(event.body);
 
-		const paymentIntent = await stripe.paymentIntent.create({
+		const paymentIntent = await stripe.paymentIntents.create({
 			amount,
 			currency: "usd",
 			payment_method_types: ["card"]
